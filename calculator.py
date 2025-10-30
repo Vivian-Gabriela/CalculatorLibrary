212fc70064597ce19626e2fb387e9b1df35acfea
Vivian Gabriela <gabrielavivian981@gmail.com>
2025-10-30 15:09:27 -0300

    Adicionando funcao de divisao

--- calculator.py
+++ calculator.py
@@ -8,3 +8,10 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+def division(first_term, second_term):
+    if second_term == 0:
+        return "Error: Division by zero"
+    resultado = first_term / second_term
+
+    return round(resultado, 2)
