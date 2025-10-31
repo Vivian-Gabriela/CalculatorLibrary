0a0206aeb9048f5905791f70207550e7f19d4e58
Vivian Gabriela <gabrielavivian981@gmail.com>
2025-10-31 15:07:59 -0300

    Induzindo erro na funcao subtracao

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ def add(first_term, second_term):
 
 
 def subtract(first_term, second_term):
-    return first_term - second_term
+    return second_term - first_term
 
 
 def multiply(first_term, second_term):
