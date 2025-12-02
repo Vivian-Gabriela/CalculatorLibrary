8034332bfc6847e97ebe6bdd72c4d268d9410906
Vivian Gabriela <gabrielavivian981@gmail.com>
2025-12-02 15:31:00 -0300

    Corrigindo erro no comando assert

--- calculator.py
+++ calculator.py
@@ -11,7 +11,7 @@ def multiply(first_term, second_term):
 
 def division(first_term, second_term):
     if second_term == 0:
-        return "Error"
+        return "Error: Division by zero"
     resultado = first_term / second_term
 
     return round(resultado, 2)
